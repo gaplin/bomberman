08023cb1b4001628fccb495b35c1f69dd15e377b
miloszjanuszewski <milosz.januszewski@student.uj.edu.pl>
2019-05-12 16:50:47 +0200

    and map..

---
+++ core/assets/map/bomb64.tsx
@@ -0,0 +1,3 @@
+<tileset version="1.2" tiledversion="2019.04.18" name="bomb64" tilewidth="64" tileheight="64" tilecount="256" columns="16">
+ <image source="terrain.png" width="1024" height="1024"/>
+</tileset>
